10cd161038621539d02fe07de60a3d847fb4743c
407185033 <407185033@qq.com>
2019-06-26 17:42:14 +0800

    1

---
+++ src/components/qt/danxuan/danxuan.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  props: {
+    DataInfo: Object
+  },
+  data() {
+    return {}
+  },
+  computed: {},
+  mounted() {},
+  methods: {
+    divclick(index) {}
+  },
+  watch: {
+    dataSet: {
+      handler(nVal, oVal) {},
+      immediate: true,
+      deep: true
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
+
