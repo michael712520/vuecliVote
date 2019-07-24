b33e725b827be778670ad3ee6e10b99c10a6a241
40718533 <40718533@qq.com>
2019-07-25 01:11:16 +0800

    1

--- src/api/latitudeDetail.js
+++ src/api/latitudeDetail.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
-import { axios } from '@/utils/request'
+import {
+  axios
+} from '@/utils/request'
 import defaultSettings from '@/config/defaultSettings'
 import request from './comm/request'
 
@@ -16,31 +18,52 @@ export const config = {
   UpdateItemMbDetailItem: `POST ${baseUrl}/api/LatitudeDetail/UpdateItemMbDetailItem`
 }
 export async function Add(params) {
-  const { success, data } = await request(config.Add, params)
+  const {
+    success,
+    data
+  } = await request(config.Add, params)
   return data
 }
 export async function Update(params) {
-  const { success, data } = await request(config.Update, params)
+  const {
+    success,
+    data
+  } = await request(config.Update, params)
   return data
 }
 
 export async function List(params) {
-  const { success, data } = await request(config.List, params)
+  const {
+    success,
+    data
+  } = await request(config.List, params)
   return data
 }
 export async function Delete(params) {
-  const { success, data } = await request(config.Delete, params)
+  const {
+    success,
+    data
+  } = await request(config.Delete, params)
   return data
 }
 export async function GetPicker(params) {
-  const { success, data } = await request(config.GetPicker, params)
+  const {
+    success,
+    data
+  } = await request(config.GetPicker, params)
   return data
 }
 export async function GetListLat(params) {
-  const { success, data } = await request(config.GetListLat, params)
+  const {
+    success,
+    data
+  } = await request(config.GetListLat, params)
   return data
 }
 export async function UpdateItemMbDetailItem(params) {
-  const { success, data } = await request(config.UpdateItemMbDetailItem, params)
+  const {
+    success,
+    data
+  } = await request(config.UpdateItemMbDetailItem, params)
   return data
 }
--- src/config/router.config.js
+++ src/config/router.config.js
@@ -1,34 +1,42 @@
 // eslint-disable-next-line
 /* eslint-disable */
-import { UserLayout, BasicLayout, RouteView, BlankLayout, PageView, PreviewLayout, BasicLayoutList } from '@/layouts'
-import { bxAnaalyse } from '@/core/icons'
+import {
+  UserLayout,
+  BasicLayout,
+  RouteView,
+  BlankLayout,
+  PageView,
+  PreviewLayout,
+  BasicLayoutList
+} from '@/layouts'
+import {
+  bxAnaalyse
+} from '@/core/icons'
 
 export const asyncRouterMap = []
 /**
  * 基础路由
  * @type { *[] }
  */
-export const constantRouterMap = [
-  {
+export const constantRouterMap = [{
     path: '/user',
     component: UserLayout,
     redirect: '/user/login',
     hidden: true,
-    children: [
-      {
+    children: [{
         path: 'login',
         name: 'login',
-        component: () => import(/* webpackChunkName: "user" */ '@/views/user/Login')
+        component: () => import( /* webpackChunkName: "user" */ '@/views/user/Login')
       },
       {
         path: 'register',
         name: 'register',
-        component: () => import(/* webpackChunkName: "user" */ '@/views/user/Register')
+        component: () => import( /* webpackChunkName: "user" */ '@/views/user/Register')
       },
       {
         path: 'register-result',
         name: 'registerResult',
-        component: () => import(/* webpackChunkName: "user" */ '@/views/user/RegisterResult')
+        component: () => import( /* webpackChunkName: "user" */ '@/views/user/RegisterResult')
       }
     ]
   },
@@ -37,18 +45,16 @@ export const constantRouterMap = [
     path: '/test',
     component: BlankLayout,
     redirect: '/test/home',
-    children: [
-      {
-        path: 'home',
-        name: 'TestHome',
-        component: () => import('@/views/Home')
-      }
-    ]
+    children: [{
+      path: 'home',
+      name: 'TestHome',
+      component: () => import('@/views/Home')
+    }]
   },
 
   {
     path: '/404',
-    component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/404')
+    component: () => import( /* webpackChunkName: "fail" */ '@/views/exception/404')
   },
   {
     path: '/',
@@ -58,8 +64,7 @@ export const constantRouterMap = [
       title: '首页'
     },
     redirect: '/dashboard/basic-list',
-    children: [
-      {
+    children: [{
         path: '/dashboard/basic-list',
         name: 'question',
         component: () => import('@/views/tp/Clist/StandardList')
@@ -79,55 +84,61 @@ export const constantRouterMap = [
   {
     path: '/ExternalLinks/wj',
     name: 'ExternalLinks_wj',
-    component: () => import(/* webpackChunkName: "fail" */ '@/views/ExternalLinks/wj')
+    component: () => import( /* webpackChunkName: "fail" */ '@/views/ExternalLinks/wj')
   },
   {
     path: '/ExternalLinks/studentList',
     name: 'ExternalLinks_studentList',
-    component: () => import(/* webpackChunkName: "fail" */ '@/views/ExternalLinks/studentList')
+    component: () => import( /* webpackChunkName: "fail" */ '@/views/ExternalLinks/studentList')
   },
   {
     path: '/ExternalLinks/studentAndMbQuestion',
     name: 'ExternalLinks_studentAndMbQuestion',
-    component: () => import(/* webpackChunkName: "fail" */ '@/views/ExternalLinks/studentAndMbQuestion')
+    component: () => import( /* webpackChunkName: "fail" */ '@/views/ExternalLinks/studentAndMbQuestion')
   },
   {
     path: '/ExternalLinks/result',
     name: 'ExternalLinks_result',
-    component: () => import(/* webpackChunkName: "fail" */ '@/views/ExternalLinks/result')
+    component: () => import( /* webpackChunkName: "fail" */ '@/views/ExternalLinks/result')
   },
   {
     path: '/ExternalLinks/repeatwj',
     name: 'ExternalLinks_repeatwj',
-    component: () => import(/* webpackChunkName: "fail" */ '@/views/ExternalLinks/repeatwj')
+    component: () => import( /* webpackChunkName: "fail" */ '@/views/ExternalLinks/repeatwj')
+  },
+  {
+    path: '/ExternalLinks/txwj',
+    name: 'ExternalLinks_txwj',
+    component: () => import( /* webpackChunkName: "fail" */ '@/views/ExternalLinks/wj/txwj')
+  },
+  {
+    path: '/wjitem/wjitem',
+    name: 'wjitem_wjitem',
+    component: () => import( /* webpackChunkName: "fail" */ '@/views/wjitem/wjitem')
   },
   {
     path: '/ExternalLinks/studentListSimple',
     name: 'ExternalLinks_studentListSimple',
-    component: () => import(/* webpackChunkName: "fail" */ '@/views/ExternalLinks/studentListSimple')
+    component: () => import( /* webpackChunkName: "fail" */ '@/views/ExternalLinks/studentListSimple')
   },
   {
     path: '/dashboard/Question',
     name: 'question',
     component: BasicLayout,
-    children: [
-      {
-        path: '/dashboard/Question',
-        name: 'question',
-        component: () => import('@/views/tp/Quest/Question')
-      }
-    ]
+    children: [{
+      path: '/dashboard/Question',
+      name: 'question',
+      component: () => import('@/views/tp/Quest/Question')
+    }]
   },
   {
     path: '/preview',
     component: PreviewLayout,
     redirect: '/preview/index',
-    children: [
-      {
-        path: '/preview/index',
-        name: 'index',
-        component: () => import('@/views/preview/index.vue')
-      }
-    ]
+    children: [{
+      path: '/preview/index',
+      name: 'index',
+      component: () => import('@/views/preview/index.vue')
+    }]
   }
 ]
